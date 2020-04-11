8ecfc8bb9e9d37009a1cec612e5573f9368b1222
ND6354 <54520924+ND6354@users.noreply.github.com>
2020-04-10 23:50:27 -0700

    test upload

---
+++ User.java.java
@@ -0,0 +1,19 @@
+import java.util.String
+
+public class user {
+	private:
+		String firstname;
+		String lastname;
+		String PhoneNum;
+		int userID;
+		String address;
+		String city;
+		String state;
+	public:
+		paymentInfo(String fn, String ln, int credit, String exstate, int ccv) {
+			
+		}
+		savedOrder(UserCart order) {
+			
+		}
+}
